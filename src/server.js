06081aa2ca9b04e9131c08b5ad5f49f14c6cb497
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-10-05 12:12:10 +0100

    add basic backend server working

---
+++ src/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const userRouter = require("./users/routes");
+const User = require("./users/model");
+
+
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+// Creates table if not found in db
+const syncTables = () => {
+    User.sync();
+};
+
+
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        message: "API is healthy"
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}.`);
+    syncTables();
+});
